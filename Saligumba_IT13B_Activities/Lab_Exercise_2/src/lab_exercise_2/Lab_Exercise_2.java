a495e5ec4ddfbc4f9becabb503d677fab962a05e
rodel092 <rodelsaligumba09@gmail.com>
2025-02-24 11:50:37 +0800

    Added Lab Exercise

---
+++ Saligumba_IT13B_Activities/Lab_Exercise_2/src/lab_exercise_2/Lab_Exercise_2.java
@@ -0,0 +1,33 @@
+
+package lab_exercise_2;
+
+import java.util.Scanner;
+
+public class Lab_Exercise_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x, y, z;
+
+        System.out.println("Input the 1st number");
+        x = in.nextInt();
+        System.out.println("Input the 2nd number");
+        y = in.nextInt();
+        System.out.println("Input the 3rd number");
+        z = in.nextInt();
+        int greatest = x;
+
+        if (y > greatest);
+        greatest = z;
+        if (z > greatest){
+
+        }
+        System.out.println("The greatest number is " + greatest);
+       
+       
+
+    }
+}
