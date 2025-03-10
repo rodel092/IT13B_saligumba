9ad4363f9670bb619b0779f85c19dee6c62f288e
angel <sortonesangel@gmail.com>
2025-03-10 11:14:48 +0800

    Added Palindrome

---
+++ Saligumba_IT13B_Activities/PALINDROME/src/MIDTERM/PALINDROME.java
@@ -0,0 +1,30 @@
+
+package MIDTERM;
+
+import java.util.Scanner;
+
+public class PALINDROME {
+
+    public static void main(String[] args) {
+        
+        String original;
+        Scanner in= new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        original=in.nextLine();
+        
+        String reverse_original= new StringBuilder(original).reverse().toString();
+        
+        if (original.equalsIgnoreCase(reverse_original)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+        
+        
+        
+        
+        
+        
+    }
+
+}
